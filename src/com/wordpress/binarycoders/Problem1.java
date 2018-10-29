c76e8b421875db24e4f66dea80ab3e5e5a95f980
fjavierm <fco.javier.marquez@gmail.com>
2018-10-29 19:25:58 +0000

    Project Euler - Problem 1: Multiples of 3 and 5

---
+++ src/com/wordpress/binarycoders/Problem1.java
@@ -0,0 +1,24 @@
+package com.wordpress.binarycoders;
+
+import java.util.stream.IntStream;
+
+/**
+ * Multiples of 3 and 5
+ *
+ * Problem 1
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+public class Problem1 {
+
+	public static void main(String[] args) {
+		System.out.println(Problem1.solve());
+	}
+
+	private static int solve() {
+		return IntStream.range(1, 1000)
+				.filter(n -> n % 3 == 0 || n % 5 == 0)
+				.sum();
+	}
+}
